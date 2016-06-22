7aabec91ec0d51ea2f6dd1107bcefa289ffb05ba
yangli_sky@163.com <yangli_sky@163.com>
2016-06-22 09:48:10 +0800

    a

---
+++ SignalRService/SignalRService/Controller/PushController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SignalRService.Controller
+{
+    public class PushController: ApiController
+    {
+        [HttpGet]
+        public string Test(string msg)
+        {
+            if (string.IsNullOrEmpty(msg))
+            {
+                return "no";
+            }
+            IHubContext chat = GlobalHost.ConnectionManager.GetHubContext<LogHub>();
+            chat.Clients.All.notice(msg);
+            return "ok";
+        }
+    }
+}
---
+++ SignalRService/SignalRService/Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using Microsoft.Owin.Cors;
+using Microsoft.AspNet.SignalR;
+
+[assembly: OwinStartup(typeof(SignalRService.Startup))]
+
+namespace SignalRService
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // 有关如何配置应用程序的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkID=316888
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+
+
+            app.Map("", map => {
+                map.UseCors(CorsOptions.AllowAll);
+                var hubConfig = new HubConfiguration() { EnableJSONP = true };
+                map.RunSignalR(hubConfig);
+            });
+        }
+    }
+}
