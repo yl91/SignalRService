fa0331fa4233960ee09b9c334b9f85e7056e51af
yangli_sky@163.com <yangli_sky@163.com>
2016-06-27 17:14:15 +0800

    aaa

--- SignalRService/SignalRService/Startup.cs
+++ SignalRService/SignalRService/Startup.cs
@@ -14,7 +14,7 @@ namespace SignalRService
     {
         public void Configuration(IAppBuilder app)
         {
-            // 有关如何配置应用程序的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkID=316888
+            // 有关如何配置应用程序的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkID=316888 aaaaa
             HttpConfiguration config = new HttpConfiguration();
             WebApiConfig.Register(config);
             app.UseWebApi(config);
